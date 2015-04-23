9bf18ca5180876d81dc2d4b53682ca7e2882908d
GPmap <g.pourriere@map-architecture.fr>
2015-04-23 16:31:12 +0200

    mise à jour des styles et single
    Styles remaniés et mise à jour de single.php, page par défaut des
    articles unique. Elle est utilisée tant que les single-....php ne sont
    pas reconnues.

--- single-AMA.php
+++ single-AMA.php
@@ -1,27 +1,33 @@
 <?php
+/**
+ * The Template for displaying all single posts
+ *
+ * @package WordPress
+ * @subpackage Twenty Twelve_map
+ * @since Twenty Twelve 1.0
+ */
 
 get_header(); ?>
-	<br />
-	<div id="primary" class="site-content">single-cat-37.php
+	<div id="primary" class="site-content">
 		<div id="content" role="main">
+			<h1>single-perspectives.php</h1>
 			<?php while ( have_posts() ) : the_post(); ?>
+				<!-- TITLE -->
+				<?php the_title(); ?>
 				
-				<h1><?php echo the_title();?></h1>
-				
-				<?php get_template_part( 'content', get_post_format() ); ?>
+				<!-- CONTENT -->
+				<?php the_content(); ?>
 
+				<!-- NAVIGATION -->
 				<nav class="nav-single">
 					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
 					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
 					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
 				</nav><!-- .nav-single -->
 
-				<?php comments_template( '', true ); ?>
-
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
-
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -9,66 +9,29 @@
 
 get_header(); ?>
 <style>
-.conteneur_light_box
-{
-	float: left;
-	width: 70%;
-	max-width: 822px;
-	margin-left: 290px;
-	margin-top: 20px;
-}
-#visuel_carte
-{
-	display: inline-block;
-	width: 50%;
-	max-width : 400px;
-	margin-right: 20px;
-}
-.conteneur_visuels
-{
-	display: inline-block;
-	vertical-align: top;
-	float: left;
-}
-.visuel_light_box
-{
-	width: 217px;
-	vertical-align: top;
-	float: left;
-	display: block;
-	clear: right;
-}
-#gallery-info
-{
-  float: right;
-  width: 30%;
-  display: inline-block;
-  max-height: 400px !important;
-
-}
 .gallery-item
 {
-  /* float: left; */
-  margin-top: 0px!important;
-  /* text-align: center; */
-  /* width: 33.3333%!important; */
-  display: inline-block;
+	margin-top: 0px!important;
+	display: inline-block;
 }
+
 .gallery-item img
 {
 	border: none!important;
 	padding: 0px!important;
 	width: 100%!important
 }
+
 .gallery .gallery-icon img
 {
 	max-width: 100%;
 }
+
 .gallery-item a
 {
 	width: 100%;
 	height: 200px;
-	min-width:200px!important;
+	min-width: 200px!important;
 	overflow: hidden;
 }
 
@@ -76,204 +39,89 @@ get_header(); ?>
 {
 	display: none
 }
-.fiche_reference
-{
-	z-index: 10!important;
-	position: fixed;
 
-	top: 275px;
-	left: 70px;
-	width: 240px;
-	margin: 0px;
-	float: right;
+/*------------------------------------*/
+
+
+#gallery-info_single
+{
+	float: left;
+	width: 30%;
+	display: inline-block;
+	max-height: 400px !important;
+	margin-left: 50px ;
 }
-.conteneur_fiche_details
+
+.conteneur_gallerie_WP
 {
 	float: left;
 	width: 100%!important;
-	/* padding-bottom: 10px; */
-  /* padding-top: 60px; */
-  /* border: 1px solid #666; */
 	background-color: #999;
+	display: inline-block;
 }
 
-div.ref-content
+.conteneur_article_WP
 {
+	width: 50%;
+	padding: 0px;
+	display: inline-block;
 	float: left;
-	position: relative;
+	margin: 0px !important;
+}
+
+#titre_article_WP
+{
+	padding: 30px;
+}
+
+#corps_article_WP
+{
+	width: 95% !important;
+	padding-top: 30px;
+	padding-right: 30px;
+	padding-bottom: 30px;
+	padding-left: 60px;
+}
+
+.article_WP
+{
 	width: 100%;
 }
-div.ref-background
-{
-	position: relative;
-	float: left;
-	width: 100%
-}
-div.ref-background img
-{
-	width: 100%;
-}
-div.ref-description
-{
-	position: fixed;
-	top: 200px;
-	left: 20px;
-	z-index: 10!important;
-	width: 30%;
-}
-div.ref-carte
-{
-  float: right;
-  width: 40%;
-  height: 100%;
-  margin-top: 0px;
-  /* margin-right: 10%; */
-  margin-bottom: 20px;
-  /* margin-left: 20px; */
-}
-
-div.gallery
-{
-
-}
-.parallax-window
-{
-	min-height: 1200px;
-	background: transparent;
-	position: relative;
-	float: left;
-	width: 100%;
-}
-.parallax-mirror
-{
-	z-index: 10!important
-}
-div#static-description
-{
-	float: left;
-	width: 30% !important;
-	padding-top: 0px;
-	padding-right: 0px;
-	padding-bottom: 0px;
-	padding-left: 0px;
-	background-color: #999;
-	
-}
-#static-description fiche_reference d-tails {
-  height: 260px;
-  /* width: 100%; */
-  border: 2px none #930;
-  background-color: #FFF;
-  padding: 10px;
-  margin-bottom: 0px;
-}
-
 </style>
 
 <!-- début de la page ----------------------------------------------- -->
 <div id="primary" class="site-content">
-
-			<div id="content" role="main">
-						<?php while ( have_posts() ) : the_post(); ?>
-						<!-- script Google -->
-						<script src="http://maps.googleapis.com/maps/api/js?sensor=false" type="text/javascript"></script>
-						<!-- début du contenu de la page ---------------------------------------------- -->
-						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-									
-									<!-- reference ---------------------------------------------- -->
-									
-							<div class="ref-content">
-												<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full', false);
+ <div id="content" role="main">
+  
+  <!-- début du contenu de la page ---------------------------------------------- -->
+  
+  <?php while ( have_posts() ) : the_post(); ?>
+  <!-- début de la boucle ------ -->
+  <!-- Galerie  ---------------------------------------------- -->
+  		<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full', false);
 	$url = $thumb['0']; ?><!-- parallaxe ---------------------------------------------- -->
-												<div class="parallax-window" data-parallax="scroll" data-image-src="<?php echo $url; ?>" data-speed="0.4">
+												<div class="visuel_article_WP">
+            <img src="<?php echo $url; ?>" data-speed="0.4">
 												</div>
-												<!-- fin parallaxe ---------------------------------------------- -->
-												<!-- import html de la reference depuis ID ---------------------------------------------- -->
-												<div class="fiche_reference">
-															<?php the_content(); ?>
-												</div>
-									</div>
-									<!-- fin import html de la reference depuis ID ---------------------------------------------- -->
-									<!-- détails de la fiche : carte, photos, etc ---------------------------------------------- -->
-									<div class="conteneur_fiche_details">
-												<!-- appel de la carte ---------------------------------------------- -->
-												<?php if(get_field('adresse_de_loperation')!= '') : ?>
-												<script>
+  <div id="gallery-info_single">
+  </div>
+  <div class="conteneur_article_WP fond_blanc">
+   <div id="titre_article_WP" class="article_WP texte_blanc fond_jaune T-3">
+    <?php the_title(); ?>
+   </div>
+   <div id="corps_article_WP" class="article_WP T-5  interligne_-2">
+    <?php the_content(); ?>
+   </div>
+  </div>
+  <script>
 		jQuery(document).ready(function(){
-			$(".ref-carte").gmap3({
-				marker:{
-				  address: "<?php echo get_field('adresse_de_loperation'); ?>",
-				  data: "<?php the_title(); ?>",
-				  options:{
-					 draggable: false
-					},
-				  events:{
-					mouseover: function(marker, event, context){
-					var map = $(this).gmap3("get"),
-					infowindow = $(this).gmap3({get:{name:"infowindow"}});
-					if (infowindow){
-					  infowindow.open(map, marker);
-					  infowindow.setContent(context.data);
-					} else {
-					  $(this).gmap3({
-						infowindow:{
-						  anchor:marker, 
-						  options:{content: context.data}
-						}
-					  });
-					}
-					},
-					mouseout: function(){
-						var infowindow = $(this).gmap3({get:{name:"infowindow"}});
-						if (infowindow){
-						  infowindow.close();
-						}
-					}
-				}
-				},
-				map:{
-				  options:{
-					zoom: 16
-				  }
-				}, 
-				
-			});
-		})
-
-	</script>
-												<!-- carte ---------------------------------------------- -->
-												<?php endif;?>
-												<div id="static-description">
-												</div>
-												<?php if(get_field('adresse_de_loperation')!= '') : ?>
-												<div class="ref-carte">
-												</div>
-												<div id="gallery-info">
-												</div>
-												<?php endif;?>
-												<script>
-		jQuery(document).ready(function(){
-			$('div.gallery').appendTo('#gallery-info').show(); /*transfert gallerie wordpress vers le conteneur */
-			 $('html, body').animate({
-				scrollTop:190
-			}, 500);
-			$('#static-description').width( $('.fiche_reference').width());
-			$('#static-description').height( $('.fiche_reference').height());
-			$('.conteneur_fiche_details').height( $('.fiche_reference').height());
-			$('.conteneur_fiche_details').css({'min-height' :  $('.fiche_reference').height() + 'px'});
+			$('div.gallery').appendTo('#gallery-info_single').show(); /*transfert gallerie wordpress vers le conteneur */
 		
-			$('.ref-carte').waypoint(function(direction){ // no offset on the way down
-			   if(direction == 'down') {
-				$('.fiche_reference').appendTo('#static-description').show().css({'position':'initial','width':'100%'});
-				}else{
-				   $('.fiche_reference').css({'position':'fixed','width':'240px'}).appendTo('#static-description').show();
-				   //$('#static-description').html('');
-				}
-			},{offset:'bottom-in-view'});
 		})
 </script>
-
-												<?php 
+  
+  <!-- script pour la navigation -->
+  <?php 
 		function get_the_subcategory()
 		{
 			$categories = get_the_category();
@@ -355,34 +203,32 @@ div#static-description
 		}
 	
 	?>
-									</div>		
-									<!-- NAVIGATION précédent / suivant ----------------------------------------------  -->
-									<nav class="ref_precedent_suivant">
-												<?php
+ </div>
+ <!-- NAVIGATION précédent / suivant ----------------------------------------------  -->
+ <nav class="ref_precedent_suivant">
+  <?php
 			$previous = get_previous_subcategory_post_link();
 			$next = get_next_subcategory_post_link();
 		?>
-												<?php if($previous != false) { ?>
-												<a href="<?php echo $previous; ?>">
-															<div id="position_fleche_gauche" class="fleche_gauche">
-															</div>
-												</a>
-												<?php } ?>
-												<?php if($next != false) { ?>
-												<a href="<?php echo $next; ?>">
-															<div id="position_fleche_droite" class="fleche_droite">
-															</div>
-												</a>
-												<?php }  ?>
-									</nav>
-									<!-- fin navigation -------------------------------- -->
-									<!-- fil d'Ariane  ----------------------------------------------  -->
-						</article>
-						<!----appel du content-ref pour mise en page personnalisée --->
-						
-						<?php endwhile; // end of the loop. ?>
-			</div>
-			<!-- #content -->
+  <?php if($previous != false) { ?>
+  <a href="<?php echo $previous; ?>">
+   <div id="position_fleche_gauche" class="fleche_gauche">
+   </div>
+  </a>
+  <?php } ?>
+  <?php if($next != false) { ?>
+  <a href="<?php echo $next; ?>">
+   <div id="position_fleche_droite" class="fleche_droite">
+   </div>
+  </a>
+  <?php }  ?>
+ </nav>
+ <!-- fin navigation -------------------------------- -->
+ 
+ </article>
+ <?php endwhile; // end of the loop. ?>
 </div>
+<!-- #content -->
+
 </div>
 <?php get_footer(); ?>
